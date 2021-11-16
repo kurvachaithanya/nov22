cbcf0b92c7c2b28795dd421736e7e40d3a287a97
K CHaitu <kchaithanya.2010@gmail.com>
2021-11-16 19:22:01 +0530

    added build file

---
+++ jenkinsfile2_build.groovy
@@ -0,0 +1,20 @@
+pipeline{
+    agent any
+    stages{
+        stage("clone code"){
+            steps{
+                println "cloning code"
+            }
+        }
+        stage("build code"){
+            steps{
+                println "maven"
+            }
+        } 
+        stage("upload artifacts to s3"){
+            steps{
+                println " uploading artifacts to s3"
+            }
+        }
+    }
+}
